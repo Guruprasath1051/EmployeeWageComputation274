e855e5bc6f1fc226d7bf8cf0fd6b3c2e2eb307aa
guruprasath <guruprasath1051@gmail.com>
2023-04-05 22:47:58 +0530

    Adding monthy working hours

--- EmployeeWageComputation274/Program.cs
+++ EmployeeWageComputation274/Program.cs
@@ -13,13 +13,14 @@ namespace EmployeeWageComputation274
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 50;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0, empWage = 0, day = 1,totalWage = 0;
+            int empHrs = 0, empWage = 0, day = 1,totalWage = 0,totalHrs =0;
 
             Console.WriteLine("Welcome To EmployeeWage Computation");
             //UC1 - EmployeeAttendence
             Random random= new Random();
-            for (day = 1; day <= MAX_WORKING_DAYS; day++)
+            while (day <=MAX_WORKING_DAYS && totalHrs<= MAX_WORKING_HRS) 
             {
                 int empAttendence = random.Next(0, 3);//0 or 1 or 2
                 switch (empAttendence)
@@ -38,11 +39,13 @@ namespace EmployeeWageComputation274
                         break;
                 }
                 empWage = empHrs * EMP_RATE_PER_HR;
-                Console.WriteLine("Day{0} Employee Wage:{1}", day, empWage);
+                Console.WriteLine("Day{0} Employee Wage:{1} TotalHrs:{2}", day, empWage,totalHrs);
                 //totalWage = totalWage + empWage;
                 totalWage += empWage;
+                day++;
+                totalHrs += empHrs;
             }
-            Console.WriteLine("Total Employee Wage for {0} days:{1}", (day - 1), totalWage);
+            Console.WriteLine("Total Employee Wage for {0} days:{1} and TotalHrs:{2}", (day - 1), totalWage,totalHrs);
             Console.ReadLine();
         }
     }
