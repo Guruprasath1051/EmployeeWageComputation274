e660b75b34ef3d9411e1b10ed923582eaef824c9
guruprasath <guruprasath1051@gmail.com>
2023-04-05 21:47:39 +0530

    Calculating employeewage

--- EmployeeWageComputation274/Program.cs
+++ EmployeeWageComputation274/Program.cs
@@ -10,21 +10,26 @@ namespace EmployeeWageComputation274
     {
         static void Main(string[] args)
         {
+            const int FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0; int empWage = 0;
 
-            int isPresent = 1;
             Console.WriteLine("Welcome To EmployeeWage Computation");
-
             //UC1 - EmployeeAttendence
-            Random random = new Random();
-            int empAttendence = random.Next(0, 2); //0 or 1
-            if (empAttendence == isPresent)
+            Random random= new Random();
+            int empAttendence = random.Next(0, 2);//0 or 1
+            if (empAttendence == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage:{0}", empWage);
             Console.ReadLine();
         }
     }
